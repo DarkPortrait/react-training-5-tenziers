dca1b1313e5a6b749d27e2f27515d89ea63f14e0
Tanvir Alam <8191415+DarkPortrait@users.noreply.github.com>
2022-06-30 17:05:16 -0400

    working tenziers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,163 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
+import Dice from "./Dice"
+import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 function App() {
+  const [diceState, setDiceState] = React.useState(allNewDice())
+  const [tenziesState, setTenziesState] = React.useState(false)
+  const [rollCountState, setRollCountState] = React.useState(0)
+  const [startTimeState, setStartTimeState] = React.useState(0)
+  const [stopTimeState, setStopTimeState] = React.useState(0)
+  const [topScoreState, setTopScoreState] = React.useState(
+    JSON.parse(localStorage.getItem("topScoreState")) || {
+      time: 0,
+      rolls: 0,
+    }
+  )
+  const confetti = tenziesState ? <Confetti /> : ""
+  const playAgain = tenziesState ? "New Game?" : "Roll"
+
+  const timer = tenziesState
+    ? Math.round((stopTimeState - startTimeState) / 1000)
+    : 0
+
+  function startTimer() {
+    if (rollCountState === 0) {
+      setStartTimeState(new Date().getTime())
+      setStopTimeState(0)
+    }
+  }
+  function stopTimer() {
+    setStopTimeState(new Date().getTime())
+  }
+
+  React.useEffect(() => {
+    const firstDieValue = diceState[0].value
+    const isHeld = diceState.every((die) => die.isHeld)
+    const everyDieTheSame = diceState.every(
+      (die) => die.value == firstDieValue
+    )
+    if (isHeld && everyDieTheSame) {
+      setTenziesState(true)
+      stopTimer()
+    }
+  }, [diceState])
+  React.useEffect(() => {
+    if (
+      (timer < topScoreState.time && timer !== 0) ||
+      topScoreState.time == 0
+    ) {
+      setTopScoreState((prevTopScoreState) => ({
+        ...prevTopScoreState,
+        time:
+          prevTopScoreState.time === 0 || prevTopScoreState.time > timer
+            ? timer
+            : prevTopScoreState.time,
+      }))
+    }
+
+    if (
+      (rollCountState < topScoreState.rolls && rollCountState !== 0) ||
+      topScoreState.rolls == 0
+    ) {
+      setTopScoreState((prevTopScoreState) => ({
+        ...prevTopScoreState,
+        rolls:
+          prevTopScoreState.rolls === 0 ||
+          prevTopScoreState.rolls > rollCountState
+            ? rollCountState
+            : prevTopScoreState.rolls,
+      }))
+    }
+
+    localStorage.setItem("topScoreState", JSON.stringify(topScoreState))
+  }, [!tenziesState])
+
+  function rollDice() {
+    if (tenziesState) {
+      setTenziesState(false)
+      setDiceState(allNewDice())
+      setRollCountState(0)
+    } else {
+      setRollCountState((prevRollCountState) => (prevRollCountState += 1))
+      setDiceState((prevDiceState) =>
+        prevDiceState.map((die) => {
+          return die.isHeld === true
+            ? die
+            : {
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid(),
+              }
+        })
+      )
+    }
+  }
+
+  const diceMapped = diceState.map((die) => (
+    <Dice
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ))
+  function holdDice(id) {
+    if (diceState.every((die) => die.isHeld == false)) {
+      startTimer()
+    }
+    setDiceState((prevDiceState) =>
+      prevDiceState.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      })
+    )
+  }
+  function allNewDice() {
+    const diceArray = []
+    for (let i = 0; i < 10; i++) {
+      diceArray.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      })
+    }
+    return diceArray
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="main--container">
+      {confetti}
+      <div className="tenzies--container">
+        <div className="highscore">
+          <p className="highscore--title">Highscores</p>
+          <p className="highscore--stats">
+            Quickest Time : {topScoreState.time}'s
+          </p>{" "}
+          <p className="highscore--stats">
+            Least Rolls : {topScoreState.rolls}
+          </p>
+        </div>
+        <div className="text--content">
+          <h1>Tenzies</h1>
+
+          <h4>
+            Roll until all dice are the same. Click each die to freeze it at
+            it's current value between rolls.
+          </h4>
+        </div>
+        <div className="dice--container">{diceMapped}</div>
+        <button onClick={rollDice} className="button--roll">
+          {playAgain}
+        </button>
+        <div className="count--container">
+          <p className="roll--count">Time Taken : {timer}'s</p>
+          <p className="roll--count"> Roll Number : {rollCountState}</p>
+        </div>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Dice.js
@@ -0,0 +1,39 @@
+import React from "react"
+import dice1 from "./img/dice1.png"
+import dice2 from "./img/dice2.png"
+import dice3 from "./img/dice3.png"
+import dice4 from "./img/dice4.png"
+import dice5 from "./img/dice5.png"
+import dice6 from "./img/dice6.png"
+
+export default function Dice(props) {
+  const styles = {
+    boxShadow: props.isHeld
+      ? "0px 0px 10px 6px rgba(89,227,145,0.75)"
+      : "0px 0px 0px 0px rgba(0,0,0,0)",
+  }
+
+  function diceSelector() {
+    if (props.value == 1) {
+      return dice1
+    } else if (props.value == 2) {
+      return dice2
+    } else if (props.value == 3) {
+      return dice3
+    } else if (props.value == 4) {
+      return dice4
+    } else if (props.value == 5) {
+      return dice5
+    } else {
+      return dice6
+    }
+  }
+
+  return (
+    <div
+      onClick={props.holdDice}
+    >
+      <img style={styles} className="dice--images" src={diceSelector()} />
+    </div>
+  )
+}
